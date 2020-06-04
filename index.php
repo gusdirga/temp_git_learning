a6a4b694baac9845936dfffee62b83bf5273d838
Gus Dirga <gusdirga@gmail.com>
2020-06-04 09:59:49 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<title>Git test Learning</title>
+</head>
+
+<body>
+	<h1>This is temporary file to learn Git. Delete later.</h1>
+	<p>This is the first update/modified to git file.</p>
+</body>
+</html>
